6a794d88e992f1a84ea96ca72c813cc409015a5b
RAFI RAHMADANI <142192147+Rafiiir@users.noreply.github.com>
2024-03-18 08:58:46 +0700

    Add files via upload

---
+++ PERTEMUAN 8/bangun_datar.py
@@ -0,0 +1,12 @@
+def luas_segitiga(alas, tinggi):
+    luas = 0.5 * alas * tinggi
+
+def luas_persegi(sisi):
+    return sisi ** 2
+
+def luas_lingkaran(radius):
+    return 3.14 * radius * radius
+
+print("Luas segitiga: ", luas_segitiga(6, 7))
+print("Luas persegi: ", luas_persegi(8))
+print("Luas lingkaran: ", luas_lingkaran(3))
---
+++ PERTEMUAN 8/perkenalan.py
@@ -0,0 +1,6 @@
+def perkenalan(nama, kelas, umur):
+    print("Nama Saya ", nama)
+    print("Saya kelas ", kelas)
+    print("Saya berusia ", umur)
+
+perkenalan("Rafi", "TKJ X A", "16 Tahun")  
